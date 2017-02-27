392d1cf3a63cdfc4d8c1cde03dfa40f034a3be2d
Dylan Vu <dylan.n.vu@gmail.com>
2017-02-27 10:33:01 -0800

    add test request

--- src/App.js
+++ src/App.js
@@ -1,11 +1,44 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {list: []};
+  }
+
+  componentDidMount() {
+    this.fetchArticlesTest();
+  }
+
+  fetchArticlesTest() {
+    axios.get(`https://newsapi.org/v1/articles?source=techcrunch&apiKey=226be9019bc144dfb7d0ebbfb4c8b0cc`)
+    .then(res => {
+      console.log(res);
+      this.setState({list: res.data.articles});
+    })
+    .catch(err => {
+      console.log(err);
+    });
+  }
+
   render() {
+    const { list } = this.state;
     return (
       <div className="App">
-        yo
+        <div>
+          { list.map(ele => (
+            <div key={ele.title}>
+              <img src={ele.urlToImage}/>
+              <p><a href={ele.url}>{ele.title}</a></p>
+              <p>{ele.author}</p>
+              <p>{ele.description}</p>
+              <p>{ele.publishedAt}</p>
+            </div>
+          ))}
+        </div>
       </div>
     );
   }
