92735f21da38a4393b0e6660ebe6a0d73446958b
Dylan Vu <dylan.n.vu@gmail.com>
2017-02-27 11:49:38 -0800

    add migration and seed for sources

---
+++ migrations/20170227113836_sources.js
@@ -0,0 +1,21 @@
+'use strict';
+
+exports.up = function(knex) {
+  return knex.schema.createTable('sources', (table) => {
+    table.increments();
+    table.string('name').notNullable();
+    table.text('description').notNullable();
+    table.string('url').notNullable();
+    table.string('category').notNullable();
+    table.string('language').notNullable();
+    table.string('country').notNullable();
+    table.string('img_small').notNullable();
+    table.string('img_medium').notNullable();
+    table.string('img_large').notNullable();
+    table.string('sortBysAvailable').notNullable();
+  });
+};
+
+exports.down = function(knex) {
+return knex.schema.dropTable('sources');
+};
