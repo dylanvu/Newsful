3b183f58669c1d777df615051ae650f338e72c7b
Dylan Vu <dylan.n.vu@gmail.com>
2017-02-27 18:37:05 -0800

    add incomplete promise handler

--- server/routes/api.js
+++ server/routes/api.js
@@ -3,31 +3,50 @@
 const router = require('express').Router();
 const axios = require('axios');
 const knex = require('../../knex');
+const request = require('request');
+
+const getArticles = function(source) {
+  const getArticlesPromise = new Promise((resolve, reject) => {
+    request(source.articlesUrl, (err, res) => {
+      if (err) {
+        return reject(err);
+      }
+      console.log(res.body);
+      resolve(res.body);
+    });
+  });
+}
 
 router.get('/', (req, res) => {
-  const testSources = ['arstechnica', 'mirror', 'abcnewsau', 'theverge'];
-
   knex('sources')
-  .select('query')
+  .select('query', 'name', 'url', 'category')
   .then(data => {
     const sources = data.map(ele => {
-      return ele.query.replace(/(-)/g, '');
+      const source = {
+        articlesUrl:
+          `https://newsapi.org/v1/articles?source=${ele.query}&apiKey=226be9019bc144dfb7d0ebbfb4c8b0cc`
+        ,
+        name: ele.name,
+        sourceUrl: ele.url,
+        category: ele.category
+      }
+      return source;
     });
-
-    const promises = sources.map(source => {
-      return axios.get(`https://newsapi.org/v1/articles?source=${source}&apiKey=226be9019bc144dfb7d0ebbfb4c8b0cc`);
-    });
-
-    return promises;
+    return sources;
   })
-  .then(promises => {
-    return Promise.all(promises);
+  .then(sources => {
+    const result = [];
+    for (const source of sources) {
+      result.push(getArticles(source));
+    }
+    console.log(result);
+    return Promise.all(result);
   })
   .then(response => {
-    // TODO: optimize
     const result = []
-    for(const sourceObj of response){
-      for(const article of sourceObj.data.articles) {
+    for (const sourceObj of response) {
+
+      for (const article of sourceObj.data.articles) {
         article.source = sourceObj.data.source;
         result.push(article)
       }
