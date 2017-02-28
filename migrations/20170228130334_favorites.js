0cfdec7b3ad57e5cd8b80b82f29af44ae6722545
will-206 <will9_30@yahoo.com>
2017-02-28 13:11:15 -0800

    add more migrations

---
+++ migrations/20170228130334_favorites.js
@@ -0,0 +1,21 @@
+'use strict';
+
+exports.up = function(knex) {
+  return knex.schema.createTable('favorites', (table) => {
+    table.increments();
+    table.string('author').notNullable();
+    table.text('description').notNullable();
+    table.date('publishedAt').notNullable();
+    table.string('sourceCategory').notNullable();
+    table.string('sourceName').notNullable();
+    table.string('sourceUrl').notNullable();
+    table.string('title').notNullable();
+    table.string('url').notNullable();
+    table.string('urlToImage').notNullable();
+    table.timestamps(true, true);
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTable('favorites');
+};
