1c35159bdb10c41981bedc1e4722df9fd9675df1
Dylan Vu <dylan.n.vu@gmail.com>
2017-03-02 12:45:51 -0800

    add get all articles route

--- server/routes/articles.js
+++ server/routes/articles.js
@@ -40,6 +40,34 @@ const authorize = function(req, res, next) {
   });
 };
 
+router.get('/allArticles', (req, res, next) => {
+  knex('sources')
+  .select('sources.query', 'sources.name', 'sources.url', 'sources.category')
+  .then(sources => {
+    const toResolve = sources.map(source => {
+      const url = `https://newsapi.org/v1/articles?source=${source.query}&apiKey=${process.env.API_KEY}`
+
+      return getArticles(url ,source);
+    });
+
+    return Promise.all(toResolve);
+  })
+  .then(articles => {
+    articles = articles.reduce((acc, arr) => {
+      return acc.concat(arr);
+    }, [])
+    .sort((a, b) => {
+      return new Date(b.publishedAt) - new Date(a.publishedAt);
+    });
+
+    res.send(articles);
+  })
+  .catch(err => {
+    next(err);
+  });
+});
+
+
 router.get('/articles', authorize, (req, res, next) => {
   knex('subscriptions')
   .select('sources.query', 'sources.name', 'sources.url', 'sources.category')
