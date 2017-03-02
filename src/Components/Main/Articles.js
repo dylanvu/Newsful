b61b89b1b1bcbfa9af2550a39982c48a0e8d584c
Dylan Vu <dylan.n.vu@gmail.com>
2017-03-01 20:10:47 -0800

    refactor articles component

--- src/Components/Main/Articles.js
+++ src/Components/Main/Articles.js
@@ -1,12 +1,21 @@
 import React from 'react';
-import Article from './Article';
 
 function Articles(props) {
   const { articles } = props;
   return (
     <div className="Articles">
       { articles.map(article => (
-        <Article article={article} key={article.title}/>
+        <div key={article.title}>
+          <div className="Article">
+            {/* <img src={article.urlToImage} alt={article.title}/> */}
+            <p><a href={article.url}>{article.title}</a></p>
+            <p>{article.author}</p>
+            <p>{article.description}</p>
+            <p>{article.publishedAt}</p>
+            <p><a href={article.sourceUrl}>{article.sourceName}</a></p>
+          </div>
+          <hr></hr>
+        </div>
       ))}
     </div>
   );
